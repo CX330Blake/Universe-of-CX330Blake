800b18bc63d516d69731ec05733925f21eba3ac7
DESKTOP-COR4OEI\陳子雋 <timchen975@gmail.com>
2024-01-27 23:08:46 +0800

    2024/01/27

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-    
+const music = document.getElementById("universe");
+const mute_icon = document.getElementById("mute");
+const play_icon = document.getElementById("play");
+mute_icon.addEventListener("click", function () {
+    if (music.onpause()) {
+        music.onplay();
+        play_icon.style.display = "inline";
+        mute_icon.style.display = "none";
+    }
+});
