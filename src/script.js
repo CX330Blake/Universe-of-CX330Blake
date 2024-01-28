76140912c4f508d77c37f74b5148b082a4e54878
DESKTOP-COR4OEI\陳子雋 <timchen975@gmail.com>
2024-01-28 16:59:29 +0800

    2024/01/28

--- src/script.js
+++ src/script.js
@@ -2,9 +2,16 @@ const music = document.getElementById("universe");
 const mute_icon = document.getElementById("mute");
 const play_icon = document.getElementById("play");
 mute_icon.addEventListener("click", function () {
-    if (music.onpause()) {
-        music.onplay();
+    if (music.paused) {
+        music.play();
         play_icon.style.display = "inline";
         mute_icon.style.display = "none";
+    }
+});
+play_icon.addEventListener("click", function () {
+    if (!music.paused) {
+        music.pause();
+        play_icon.style.display = "none";
+        mute_icon.style.display = "inline";
     }
 });
