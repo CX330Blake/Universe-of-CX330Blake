3ca4fc2831a2a3dcdeb69d73a02e4e1967e1c544
DESKTOP-COR4OEI\陳子雋 <timchen975@gmail.com>
2024-02-04 04:35:33 +0800

    2024/02/04

--- src/script.js
+++ src/script.js
@@ -72,4 +72,6 @@ to_button(fb, "https://www.facebook.com/profile.php?id=100011698690769");
 to_button(linkedin, "https://www.linkedin.com/in/%E5%AD%90%E9%9B%8B-%E9%99%B3-11087825b/");
 to_button(gmail, "", "timchen975@gmail.com");
 
-//增加點擊空白處特效
+// 增加點擊空白處特效
+// 增加回到頁面頂端
+// 增加hackmd api -> https://medium.com/starbugs/%E7%94%A8-hackmd-%E8%88%87-github-action-%E6%89%93%E9%80%A0%E4%BD%A0%E7%9A%84%E9%9D%9C%E6%85%8B%E7%B6%B2%E7%AB%99-%E7%B7%9A%E4%B8%8A%E6%96%87%E7%AB%A0%E7%B7%A8%E8%BC%AF%E5%B9%B3%E5%8F%B0-1d9b1a663e18
